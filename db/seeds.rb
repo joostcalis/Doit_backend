2ffebefd7a57f797922ec249e96b59d325728d86
joostcalis <joost_calis@hotmail.com>
2016-02-23 11:36:36 +0100

    added homepage for api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Task.create([
+{title: "huiswerk"},
+{title: "api maken", finished: true}
+])
