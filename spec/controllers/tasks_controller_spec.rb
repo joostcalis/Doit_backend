3a8124915a94727cf3d7998579ed3c4dfe29bf9a
joostcalis <joost_calis@hotmail.com>
2016-02-23 12:06:18 +0100

    added some Rspec tests

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
+  describe TasksController do
+  render_views # <-- You need this to test your json response!
 
+  it "should render successfully" do
+    get :index, :format => :json
+    response.should be_success
+  end
+
+#   it "should fail when code is not present" do
+#   	get 'some_endpoint', :format => :json
+#   	response.should be_success
+#
+#   	# Parse json response
+#   	body = JSON.parse(response.body)
+#
+#   	# Test the json structure
+#   	body.should include('status')
+#   	status = body['status']
+#   	status.should include('code')
+#   	status.should include('message')
+#   	status.should include('endpoint')
+#
+#   	# Test the response values
+#   	status['code'].should eq(600)
+#   	status['message'].should eq('Code is required')
+#   end
+# end
+end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+
+  describe "default finished false?" do
+    let(:task) { Task.create(title: "test", body: "this is a test") }
+
+    subject { task.finished }
+
+    it { is_expected.to be false }
+  end
 end
