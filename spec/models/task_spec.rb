f260f6efb542c25881d2889cc6305802e295746c
joostcalis <joost_calis@hotmail.com>
2016-02-25 23:39:19 +0100

    somehow task model didnt belong to project model yet, fixed that

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,11 +4,19 @@ RSpec.describe Task, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
 
 
-  describe "default finished false?" do
-    let(:task) { Task.create(title: "test", body: "this is a test") }
+#   describe "default finished false?" do
+#     let(:task) { Task.create(title: "test", body: "this is a test") }
+#
+#     subject { task.finished }
+#
+#     it { is_expected.to be false }
+#   end
+# end
 
-    subject { task.finished }
 
-    it { is_expected.to be false }
+
+ # RSpec.describe Todo, type: :model do
+  describe "relations" do
+     it { is_expected.to belong_to(:project) }
+   end
   end
-end
